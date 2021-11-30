7b936a2bda13f1529437b742c496157e41dad2d3
kaindy7633 <kaindy7633@gmail.com>
2021-11-30 21:07:29 +0800

    完成登录页

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,19 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+import Home from '@/views/home/Home.vue'
+import Login from '@/views/login/Login.vue'
 
-const routes: Array<RouteRecordRaw> = []
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
+  }
+]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
